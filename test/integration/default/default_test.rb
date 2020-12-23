42a24b851d56754e08cf0b2f2ddb8aea898f9161
byplayer <byplayer100@gmail.com>
2020-12-24 00:03:57 +0900

    :+1: Add /etc/foo file test
    Add test to verify /etc/foo
    - file exists
    - owned by root
    - grouped by root
    - mode 644

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,16 +1,8 @@
 # InSpec test for recipe foobar::default
 
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('/etc/foo') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 644 }
 end
