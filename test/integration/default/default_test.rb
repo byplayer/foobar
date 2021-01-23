e8f75c4327a473b41986b1feee3d13f33d9b5798
byplayer <byplayer100@gmail.com>
2021-01-24 01:38:18 +0900

    :+1: Fix mode test bug
    Mode test is failed even if mode is correct.
    Mode should be set as octal number, but I set decimal number so I fix it.

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -4,5 +4,6 @@ describe file('/etc/foo') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-  it { should be_mode 644 }
+  it { should be_mode 0o644 }
+  its('content') { should match('bar') }
 end
